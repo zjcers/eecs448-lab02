34f269c22ee64a87160614b1b3f34a3f66d08d11
Zane Cersovsky <zjcersovsky@gmail.com>
2017-08-28 13:31:39 -0500

    Implemented LinkedList.size()

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,10 +29,7 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
-	/** TODO 
-		Fix this method
-	*/
-	return(0);
+	return m_size;
 }
 
 template <typename T>
